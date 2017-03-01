efb57e0c8f25e25c9e246e2f26712dae61a7855c
Tiago Henrique <tiagocouto@Tiagos-iMac.local>
2017-03-01 12:52:50 -0800

    convertion

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var celsiusLbl: UILabel!
+    @IBOutlet var fahrenTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateCelsius()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahreinValue: Measurement<UnitTemperature>?{
+        didSet{
+            updateCelsius()
+        }
     }
-
+    var celsiusValue: Measurement<UnitTemperature>?{
+        if let fahreinValue = fahreinValue {
+            return fahreinValue.converted(to: .celsius)
+        }else{
+            return nil
+        }
+    }
+    
+    func updateCelsius(){
+        if let celsiusValue = celsiusValue{
+            celsiusLbl.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        }else{
+            celsiusLbl.text = "???"
+        }
+    }
+    
+    @IBAction func finishEditingTextField(_ textField: UITextField){
+        if let text = textField.text, let value = Double(text){
+            fahreinValue = Measurement(value: value, unit: .fahrenheit)
+        }else{
+            fahreinValue = nil
+        }
+    }
+    
+    @IBAction func dissmisKeyboard(_ sender: UITapGestureRecognizer){
+        fahrenTextField.resignFirstResponder()
+    }
+    
+    
 
 }
 
