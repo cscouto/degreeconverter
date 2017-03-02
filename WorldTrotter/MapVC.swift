b09606ded97e5814d3a0a2d20952790ac0146e09
Tiago Henrique <tiagocouto@Tiagos-iMac.local>
2017-03-02 08:57:55 -0800

    constraints

--- WorldTrotter/MapVC.swift
+++ WorldTrotter/MapVC.swift
@@ -7,7 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController{
-
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard","Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        
+        topConstraint.isActive = true
+        trailingConstraint.isActive = true
+        leadingConstraint.isActive = true
+    }
 }
